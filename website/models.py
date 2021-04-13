9d022f6cf953b6341b74d7a490c227a281807745
Rokas <rookas.rudzenskas@gmail.com>
2021-04-13 13:22:39 +0300

    Rokas

--- website/models.py
+++ website/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 # Create your models here.
 
-class Members(models.Model):
+class Member(models.Model):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
     passwd = models.CharField(max_length=50)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Member
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    all_members = Member.objects.all
+    return render(request, 'home.html', {
+        'all': all_members
+    })
+
