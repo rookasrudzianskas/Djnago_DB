e71aebb0d2ebcccbb59dea0439709ea0e637f77a
Rokas <rookas.rudzenskas@gmail.com>
2021-04-13 12:10:22 +0300

    Rokas

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Members(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
